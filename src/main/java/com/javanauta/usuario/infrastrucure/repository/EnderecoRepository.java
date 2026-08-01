c70906ecd4c2cb6721c3ecc972b58f4add9bdf25
JLMeirelesjr <joseluciomeireles.junior!gmail.com>
2026-08-01 11:55:43 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastrucure/repository/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package com.javanauta.usuario.infrastrucure.repository;
+
+import com.javanauta.aprendendo_spring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+
+}
